8078467dcb2c664184d0750c7572eaffa52f1850
leoluce <leonardo.carvalho-lucena@itau-unibanco.com.br>
2021-03-06 00:16:29 -0300

    Initial commit

---
+++ src/test/java/br/com/mania/churrasco/maniaChurrasco/ManiaChurrascoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mania.churrasco.maniaChurrasco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManiaChurrascoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
